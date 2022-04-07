8bf302085c4a3be8b790706c4f66066790bb5fb6
Beernaardm <b.maglaviceanu@yahoo.com>
2022-04-07 17:23:07 +0300

    Curs 3

--- Curs 3.py
+++ Curs 3.py
@@ -1,1 +1,70 @@
-print("Hello")
+# #CURS 3
+# import random
+#
+# choice = [x for x in range(11)]
+# print(choice)
+#
+# while True:
+#     random_choice = random.choice(choice)
+#     if random_choice % 3 == 0:
+#         break
+#     # print(f'random_choice:{random_choice}')
+#     print('random_choice: {} out of {}'.format(random_choice, choice))
+#
+# print(f'last_random_choice:{random_choice}')
+#
+#
+# #def my_sum(a, b):
+#  #   return a + b
+#
+# #print(my_sum(1, 2))
+#
+# def my_function(list_param):
+#     list_param = list_param.copy()
+#     list_param.append(99)
+#     print(f'list_param inner function {list_param}')
+#
+# my_list = [1, 2, 3]
+# my_function(my_list)
+# print(my_list)
+#
+#
+# def my_sum(*args):
+#     acc = 0
+#     for arg in args:
+#         acc += arg
+#     return acc
+#
+#
+# print(f'Suma totala: {my_sum(1, 2, 3, 4, 5, 6)}')
+#
+#
+#
+
+# my_var = input('Introduceti un nr: ')
+# try:
+#     my_int = int(my_var)
+#     print(my_int)
+# except ValueError as e:
+#     print('Introduceti un nr intreg valid!')
+# except NameError as e:
+#     print('Pt developer. ai grija la cod')
+# else:
+#     print('Ajungem aici daca nu avem exceptii!! ')
+# finally:
+#     print('Ajungem aici cu sau fara exceptii! '
+
+
+# print(dir(__builtins__))
+
+# def my_function():
+#     def my_second_function():
+#         print(f'my second function {msg}')
+#
+#     msg = 'Hello World!!!'
+#     my_second_function()
+#     print((f'my function {msg}')
+#
+#
+# my_function()
+
